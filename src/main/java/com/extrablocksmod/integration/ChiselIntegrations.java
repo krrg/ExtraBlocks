4c40834bed74d17982ffc3c0313f5d4af16ad571
Vidar Johansen <Vidar Johansen>
2016-07-04 23:51:35 +0200

    Fixed chisel integration

--- src/main/java/com/extrablocksmod/integration/ChiselIntegrations.java
+++ src/main/java/com/extrablocksmod/integration/ChiselIntegrations.java
@@ -10,6 +10,7 @@ import com.extrablocksmod.blocks.blockstates.TfcBlocktypes2;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import team.chisel.api.carving.CarvingUtils;
+import team.chisel.api.carving.CarvingUtils.SimpleCarvingGroup;
 import team.chisel.api.carving.CarvingUtils.SimpleCarvingVariation;
 import team.chisel.api.carving.ICarvingGroup;
 import team.chisel.api.carving.ICarvingRegistry;
@@ -25,25 +26,20 @@ public class ChiselIntegrations {
 		ICarvingGroup cobbleGroup = chisel.getGroup(Blocks.COBBLESTONE.getDefaultState());
 		ICarvingGroup stoneGroup = chisel.getGroup(Blocks.STONE.getDefaultState());
 		
-		List<ICarvingGroup> reverseCobbleGroups = new ArrayList<ICarvingGroup>();
-		LIst<ICarvingGroup> reverseStoneGroups = new ArrayList<ICarvingGroup>();
-		
 		for (TfcBlocktypes1 type : TfcBlocktypes1.values()) {
 			IBlockState cobblestate = ModBlocks.TfcCobble.getStateFromMeta(type.getMeta());
 			IBlockState brickState = ModBlocks.TfcBricks.getStateFromMeta(type.getMeta());
 			
-			cobbleGroup.addVariation(new SimpleCarvingVariation(cobblestate, 99));
-			stoneGroup.addVariation(new SimpleCarvingVariation(brickState, 99));
-			
-			
+			chisel.addVariation(cobbleGroup.getName(), cobblestate, 99);
+			chisel.addVariation(stoneGroup.getName(), brickState, 99);
 		}
 		
 		for (TfcBlocktypes2 type : TfcBlocktypes2.values()) {
 			IBlockState cobblestate = ModBlocks.TfcCobble2.getStateFromMeta(type.getMeta());
 			IBlockState brickState = ModBlocks.TfcBricks2.getStateFromMeta(type.getMeta());
 			
-			cobbleGroup.addVariation(new SimpleCarvingVariation(cobblestate, 99));
-			stoneGroup.addVariation(new SimpleCarvingVariation(brickState, 99));
+			chisel.addVariation(cobbleGroup.getName(), cobblestate, 99);
+			chisel.addVariation(stoneGroup.getName(), brickState, 99);
 		}
 	}
 }
