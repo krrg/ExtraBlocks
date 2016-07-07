40531c858b56cb846021092b619f534188994a24
Vidar Johansen <Vidar Johansen>
2016-07-07 21:00:11 +0200

    Fixed a problem with too few slots in chisel UI. Added smelting recipes for cobblestone and crafting of bricks from raw stone.

--- src/main/java/com/extrablocksmod/integration/ChiselIntegrations.java
+++ src/main/java/com/extrablocksmod/integration/ChiselIntegrations.java
@@ -1,9 +1,11 @@
 package com.extrablocksmod.integration;
 
 import com.extrablocksmod.blocks.ModBlocks;
+import com.extrablocksmod.blocks.blockstates.BlockStates;
 import com.extrablocksmod.blocks.blockstates.TfcBlocktypes1;
 import com.extrablocksmod.blocks.blockstates.TfcBlocktypes2;
 
+import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import team.chisel.api.carving.CarvingUtils;
@@ -18,19 +20,23 @@ public class ChiselIntegrations {
 			return;
 		}
 		
+		ICarvingGroup cobbleGroup = getGroup(chisel, "cobblestone");
+		ICarvingGroup stoneGroup = getGroup(chisel, "stone");
+		ICarvingGroup dirtGroup = getGroup(chisel, "dirt");
+		ICarvingGroup brickGroup = getGroup(chisel, "stonebrick");
 		
-		ICarvingGroup cobbleGroup = getGroup(chisel, Blocks.COBBLESTONE.getDefaultState());
-		ICarvingGroup stoneGroup = getGroup(chisel, Blocks.STONE.getDefaultState());
-		ICarvingGroup dirtGroup = getGroup(chisel, Blocks.DIRT.getDefaultState());
-		
-		ICarvingGroup sandGroup = CarvingUtils.getDefaultGroupFor(Blocks.SAND.getDefaultState().toString());
-		ICarvingGroup gravelGroup = CarvingUtils.getDefaultGroupFor(Blocks.GRAVEL.getDefaultState().toString());
+		ICarvingGroup sandGroup = getGroup(chisel, "sand");
+		ICarvingGroup gravelGroup = getGroup(chisel, "gravel");
 		
 		chisel.addGroup(sandGroup);
 		chisel.addGroup(gravelGroup);
 		
 		chisel.addVariation(sandGroup.getName(), Blocks.SAND.getDefaultState(), 1);
 		chisel.addVariation(gravelGroup.getName(), Blocks.GRAVEL.getDefaultState(), 1);
+		
+		addChiselGroupForAndesite(chisel);
+		addChiselGroupForGranite(chisel);
+		addChiselGroupForDiorite(chisel);
 		
 		for (TfcBlocktypes1 type : TfcBlocktypes1.values()) {
 			IBlockState cobblestate = ModBlocks.TfcCobble.getStateFromMeta(type.getMeta());
@@ -42,7 +48,7 @@ public class ChiselIntegrations {
 			IBlockState gravelState = ModBlocks.TfcGravel.getStateFromMeta(type.getMeta());
 			
 			chisel.addVariation(cobbleGroup.getName(), cobblestate, 99);
-			chisel.addVariation(stoneGroup.getName(), brickState, 99);
+			chisel.addVariation(brickGroup.getName(), brickState, 99);
 			chisel.addVariation(stoneGroup.getName(), smoothState, 99);
 			chisel.addVariation(stoneGroup.getName(), rawState, 99);
 			chisel.addVariation(sandGroup.getName(), sandState, 99);
@@ -60,7 +66,7 @@ public class ChiselIntegrations {
 			IBlockState gravelState = ModBlocks.TfcGravel2.getStateFromMeta(type.getMeta());
 			
 			chisel.addVariation(cobbleGroup.getName(), cobblestate, 99);
-			chisel.addVariation(stoneGroup.getName(), brickState, 99);
+			chisel.addVariation(brickGroup.getName(), brickState, 99);
 			chisel.addVariation(stoneGroup.getName(), smoothState, 99);
 			chisel.addVariation(stoneGroup.getName(), rawState, 99);
 			chisel.addVariation(sandGroup.getName(), sandState, 99);
@@ -68,13 +74,40 @@ public class ChiselIntegrations {
 			chisel.addVariation(gravelGroup.getName(), gravelState, 99);
 		}
 	}
+
+	private void addChiselGroupForDiorite(ICarvingRegistry chisel) {
+		ICarvingGroup dioriteGroup = getGroup(chisel, "diorite");
+		chisel.addVariation(dioriteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE), 1);
+		chisel.addVariation(dioriteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE_SMOOTH), 1);
+		chisel.addVariation(dioriteGroup.getName(), ModBlocks.TfcRaw.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Diorite), 2);
+		chisel.addVariation(dioriteGroup.getName(), ModBlocks.TfcSmooth.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Diorite), 2);
+		chisel.addVariation(dioriteGroup.getName(), ModBlocks.TfcBricks.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Diorite), 2);
+		chisel.addVariation(dioriteGroup.getName(), ModBlocks.TfcCobble.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Diorite), 2);
+	}
+
+	private void addChiselGroupForGranite(ICarvingRegistry chisel) {
+		ICarvingGroup graniteGroup = getGroup(chisel, "granite");
+		chisel.addVariation(graniteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE), 1);
+		chisel.addVariation(graniteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE_SMOOTH), 1);
+		chisel.addVariation(graniteGroup.getName(), ModBlocks.TfcRaw.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Granite), 2);
+		chisel.addVariation(graniteGroup.getName(), ModBlocks.TfcSmooth.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Granite), 2);
+		chisel.addVariation(graniteGroup.getName(), ModBlocks.TfcBricks.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Granite), 2);
+		chisel.addVariation(graniteGroup.getName(), ModBlocks.TfcCobble.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Granite), 2);
+	}
+
+	private void addChiselGroupForAndesite(ICarvingRegistry chisel) {
+		ICarvingGroup andesiteGroup = getGroup(chisel, "andesite");
+		chisel.addVariation(andesiteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE), 1);
+		chisel.addVariation(andesiteGroup.getName(), Blocks.STONE.getBlockState().getBaseState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE_SMOOTH), 1);
+		chisel.addVariation(andesiteGroup.getName(), ModBlocks.TfcRaw.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Andesite), 2);
+		chisel.addVariation(andesiteGroup.getName(), ModBlocks.TfcSmooth.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Andesite), 2);
+		chisel.addVariation(andesiteGroup.getName(), ModBlocks.TfcBricks.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Andesite), 2);
+		chisel.addVariation(andesiteGroup.getName(), ModBlocks.TfcCobble.getBlockState().getBaseState().withProperty(BlockStates.TfcStones1, TfcBlocktypes1.Andesite), 2);
+	}
 	
-	private ICarvingGroup getGroup(ICarvingRegistry chisel, IBlockState state) {
-		ICarvingGroup group = chisel.getGroup(state);
-		if (group != null) {
-			return group;
-		}
-		
-		return CarvingUtils.getDefaultGroupFor(state.toString());
+	private ICarvingGroup getGroup(ICarvingRegistry chisel, String name) {
+		ICarvingGroup group = CarvingUtils.getDefaultGroupFor(name);
+		chisel.addGroup(group);
+		return group;
 	}
 }
--- src/main/java/com/extrablocksmod/recipes/ModRecipes.java
+++ src/main/java/com/extrablocksmod/recipes/ModRecipes.java
@@ -1,7 +1,57 @@
 package com.extrablocksmod.recipes;
 
+import com.extrablocksmod.blocks.ModBlocks;
+import com.extrablocksmod.blocks.blockstates.TfcBlocktypes1;
+import com.extrablocksmod.blocks.blockstates.TfcBlocktypes2;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class ModRecipes {
 
 	public static void setup() {
+		addSmeltingForCobblestoneToRawStone();
+		addCraftingOfRawStoneToBricks();
+	}
+
+	private static void addCraftingOfRawStoneToBricks() {
+		for (TfcBlocktypes1 type : TfcBlocktypes1.values()) {
+			IBlockState brickState = ModBlocks.TfcBricks.getStateFromMeta(type.getMeta());
+			IBlockState rawState = ModBlocks.TfcRaw.getStateFromMeta(type.getMeta());
+			
+			ItemStack output = new ItemStack(brickState.getBlock(), 1, type.getMeta());
+			
+			GameRegistry.addShapedRecipe(output, "RR", "RR", 'R', new ItemStack(rawState.getBlock(), 1, type.getMeta()));
+		}
+		
+		for (TfcBlocktypes2 type : TfcBlocktypes2.values()) {
+			IBlockState brickState = ModBlocks.TfcBricks2.getStateFromMeta(type.getMeta());
+			IBlockState rawState = ModBlocks.TfcRaw2.getStateFromMeta(type.getMeta());
+			
+			ItemStack output = new ItemStack(brickState.getBlock(), 1, type.getMeta());
+			
+			GameRegistry.addShapedRecipe(output, "RR", "RR", 'R', new ItemStack(rawState.getBlock(), 1, type.getMeta()));
+		}
+	}
+	
+	private static void addSmeltingForCobblestoneToRawStone() {
+		for (TfcBlocktypes1 type : TfcBlocktypes1.values()) {
+			IBlockState cobbleState = ModBlocks.TfcCobble.getStateFromMeta(type.getMeta());
+			IBlockState rawState = ModBlocks.TfcRaw.getStateFromMeta(type.getMeta());
+			
+			ItemStack inStack = new ItemStack(cobbleState.getBlock(), 1, type.getMeta());
+			ItemStack outStack = new ItemStack(rawState.getBlock(), 1, type.getMeta());
+			GameRegistry.addSmelting(inStack, outStack, 1);
+		}
+		
+		for (TfcBlocktypes2 type : TfcBlocktypes2.values()) {
+			IBlockState cobbleState = ModBlocks.TfcCobble2.getStateFromMeta(type.getMeta());
+			IBlockState rawState = ModBlocks.TfcRaw2.getStateFromMeta(type.getMeta());
+			
+			ItemStack inStack = new ItemStack(cobbleState.getBlock(), 1, type.getMeta());
+			ItemStack outStack = new ItemStack(rawState.getBlock(), 1, type.getMeta());
+			GameRegistry.addSmelting(inStack, outStack, 1);
+		}
 	}
 }
